5ed0649192865eeb2eea1be257889b55d4f672b1
lovanto <rifkylovanto@gmail.com>
2020-06-18 10:36:21 +0700

    homeuser to html

--- chat/post.php
+++
@@ -1,9 +0,0 @@
-<?
-session_start();
-if(isset($_SESSION['name'])){
-    $text = $_POST['text'];
-    $fp = fopen("../log.html", 'a');
-    fwrite($fp, "<div class='msgln'>(".date("Y-M-D / H:i:s").") <b>".$_SESSION['name']."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
-    fclose($fp);
-}
-?>
