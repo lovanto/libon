2cf73cb01856fd2a80c4b9f878c93f19e99b670c
lovanto <rifkylovanto@gmail.com>
2020-06-18 11:40:06 +0700

    delete some php

--- chatuser/post.php
+++
@@ -1,9 +0,0 @@
-<?
-session_start();
-if(isset($_SESSION['user'])){
-    $text = $_POST['text'];
-    $fp = fopen("../log.html", 'a');
-    fwrite($fp, "<div class='msgln'>(".date("Y-M-D / H:i:s").") <b>".$_SESSION['user']."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
-    fclose($fp);
-}
-?>
